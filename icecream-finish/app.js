0fb8178d005907a1f79fc702b7c1fa76387732f7
J Silverstein <jess.pebble@gmail.com>
2017-08-14 19:54:06 -0400

    rename folder

---
+++ icecream-finish/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+const icecreamRoutes = require('./routes/icecreamRoutes');
+app.use('/icecream', icecreamRoutes);
+
+app.use('*', (req, res) => {
+  res.status(400).json({
+    message: 'Endpoint not found!',
+  });
+});
