420be3be78ccd2669276ea6084d7a72eb60b2bbe
J Silverstein <jess.pebble@gmail.com>
2017-08-15 19:49:33 -0400

    making jsx-a11y happy

--- icecream-finish/client/src/components/Footer.jsx
+++ icecream-finish/client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      Made with ❤️ by WDI DeLorean
+      Made with <span role="img" aria-label="heart">❤️</span> by WDI DeLorean
     </footer>
   )
 }
