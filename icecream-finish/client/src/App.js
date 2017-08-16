9576a33930510ed7883df999313f8882ff808926
J Silverstein <jess.pebble@gmail.com>
2017-08-16 10:14:48 -0400

    revert app to vers with prev router

--- icecream-finish/client/src/App.js
+++ icecream-finish/client/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import axios from 'axios';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import IceCreamList from './components/IceCreamList';
+import IceCream from './components/IceCream';
 
 class App extends Component {
   render() {
@@ -17,8 +17,8 @@ class App extends Component {
       <Router>
         <div className="App">
           <Header />
-            <Route exact path='/' component={Home} />
-            <Route exact path='/ice-cream' component={IceCreamList} />
+          <Route exact path="/" component={Home} />
+          <Route exact path='/ice-cream' component={IceCreamList} />
           <Footer />
         </div>
       </Router>
