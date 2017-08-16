19532adcafc1455bde94cce4d9deaa2aa20fa9bf
J Silverstein <jess.pebble@gmail.com>
2017-08-16 10:18:00 -0400

    add ice cream

---
+++ icecream-finish/client/src/components/IceCream.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class IceCream extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        One Ice IceCream
+      </div>
+    )
+  }
+}
+
+export default IceCream;
