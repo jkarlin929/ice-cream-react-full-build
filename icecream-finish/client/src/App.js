fba250b5dfa13fa22cae4f1ef0071a2bd228d707
J Silverstein <jess.pebble@gmail.com>
2017-08-16 11:31:16 -0400

    remove axios import

--- icecream-finish/client/src/App.js
+++ icecream-finish/client/src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import axios from 'axios';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
